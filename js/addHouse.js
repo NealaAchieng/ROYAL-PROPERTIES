ee6ca05734f1f0cec6ebd32e4a97c1e3908ca093
Neala Achieng <achneala49@gmail.com>
2023-06-09 13:01:14 +0300

    removed the dash from the links

---
+++ js/addHouse.js
@@ -0,0 +1,39 @@
+const addHouse = document.querySelectorAll(".house");
+const submitHouse = document.getElementById("submit-house");
+console.log(addHouse)
+submitHouse.addEventListener("click", function (e) {
+  e.preventDefault();
+  const newHouse = {};
+  const houses=[];
+  addHouse.forEach(function (element) {
+    if (element.value === "") {
+      element.classList.add("error");
+      message.textContent = "Please fill all the fields";
+      message.classList.add("error-message");
+      return;
+    } else {
+      element.classList.remove("error");
+      message.textContent=""
+      newHouse.houseName = addHouse[0].value;
+      newHouse.housePrice = addHouse[1].value;
+      newHouse.houseLocation = addHouse[2].value;
+      newHouse.houseSize = addHouse[3].value;
+      newHouse.houseRent= addHouse[4].value;
+      newHouse.houseImages = addHouse[5].files[0].name;
+      newHouse.houseamenities = addHouse[6].value;
+
+console.log(addHouse[0].value);
+console.log(newHouse);
+    }
+  })
+// local storage
+if (localStorage.getItem ("houses")===null){
+    houses.push(newHouse);
+localStorage.setItem("houses",JSON.stringify(houses));
+}
+else{
+    const localStorageHouses=JSON.parse(localStorage.getItem("houses"))
+    localStorageHouses.push(newHouse)
+    localStorage.setItem("houses",JSON.stringify(localStorageHouses))
+}
+})
--- js/app.js
+++ js/app.js
@@ -1,41 +1,57 @@
 const firstName = "neala";
 console.log(firstName);
-const addHouse = document.querySelectorAll(".house");
-const submitHouse = document.getElementById("submit-house");
-console.log(addHouse)
-submitHouse.addEventListener("click", function (e) {
-  e.preventDefault();
-  const newHouse = {};
-  const houses=[];
-  addHouse.forEach(function (element) {
-    if (element.value === "") {
-      element.classList.add("error");
-      message.textContent = "Please fill all the fields";
-      message.classList.add("error-message");
-      return;
-    } else {
-      element.classList.remove("error");
-      message.textContent=""
-      newHouse.houseName = addHouse[0].value;
-      newHouse.housePrice = addHouse[1].value;
-      newHouse.houseLocation = addHouse[2].value;
-      newHouse.houseSize = addHouse[3].value;
-      newHouse.houseRent= addHouse[4].value;
-      newHouse.houseImages = addHouse[5].files[0].name;
-      newHouse.houseamenities = addHouse[6].value;
-      newHouse.houseSelect= addHouse[7].value;
-console.log(addHouse[0].value);
-console.log(newHouse);
-    }
+
+const detailsContainer =document.getElementById("details-container");
+
+
+
+function getFromLocalStorage(){
+  const houses = JSON.parse(localStorage.getItem("houses"));
+
+  houses.map(house=>{
+    const cardDetails = document.createElement("div");
+    cardDetails.setAttribute("class", "details__card");
+    const cardDetailsImg = document.createElement("div");
+    cardDetailsImg.setAttribute("class", "details__card__img");
+    cardDetails.appendChild(cardDetailsImg);
+
+    const imgH4 = document.createElement("h4");
+    imgH4.textContent = `Ksh. ${house.housePrice}`;
+    const  imgImg =document.createElement("img");
+    imgImg.setAttribute("class", "details__card__img");
+    imgImg.setAttribute("src", `images/${house.houseImages}`)
+
+    cardDetailsImg.appendChild(imgH4);
+    cardDetailsImg.appendChild(imgImg);
+    const div = document.createElement("div");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    p1.textContent=house.houseAmenities;
+    p2.textContent=house.houseLocation;
+    p3.textContent=house.houseSize;
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    const button = document .createAttribute("button");
+    button.textContent = "Book Now"
+
+
+    cardDetails.appendChild(cardDetailsImg);
+    cardDetails.appendChild(div);
+
+   detailsContainer.appendChild(cardDetails);
+
+    
+
+
   })
-// local storage
-if (localStorage.getItem ("houses")===null){
-    houses.push(newHouse);
-localStorage.setItem("houses",JSON.stringify(houses));
+
 }
-else{
-    const localStorageHouses=JSON.parse(localStorage.getItem("houses"))
-    localStorageHouses.push(newHouse)
-    localStorage.setItem("houses",JSON.stringify(localStorageHouses))
-}
-})
+
+
+getFromLocalStorage();
+
+
+
+
